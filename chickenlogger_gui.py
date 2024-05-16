b05755a34a7110d7bccd3b6370bb044ee7099d4a
Ryan Robinson <ryan.robinson@nuburu.net>
2024-05-16 00:09:32 -0600

    ITS WORKING

--- chickenlogger_gui.py
+++ chickenlogger_gui.py
@@ -9,6 +9,7 @@ import math
 import random
 import time
 import threading
+import yamlParser as yp
 
 import nidaqmx
 from nidaqmx.constants import AcquisitionType, TerminalConfiguration, ThermocoupleType, TemperatureUnits
@@ -19,27 +20,48 @@ class SignalCommunicate(QtCore.QObject):
 
 uiclass, baseclass = pg.Qt.loadUiType("MainWindow.ui")
 
+class Cfg():
+    def __init__(self):
+        pass
+
 class MainWindow(uiclass, baseclass):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        
+        # Create config file object
+        self.conf = yp.Cfg()
 
         # Filling out functionality
         self.startMonitorButton.clicked.connect(self.startMonitor)
         self.stopMonitorButton.clicked.connect(self.stopMonitor)
         self.startRecordButton.clicked.connect(self.startRecord)
         self.stopRecordButton.clicked.connect(self.stopRecord)
-
         
+        # Load and save buttons
+        self.saveButton.clicked.connect(self.saveSettings)
+        self.loadButton.clicked.connect(self.loadSettings)
 
-        # -------------------------------- ADDED CODE --------------------------------------------- #
-        
+        # Line edits
+        local_system = nidaqmx.system.System.local()
+        for device in local_system.devices:
+            self.devCombo.addItem(device.name)
+
+        # self.devBox.setValidator(QtGui.QIntValidator())
+        # self.tpChan.setValidator(QtGui.QIntValidator())
+        # self.tpTChan.setValidator(QtGui.QIntValidator())
+        # self.pdChan.setValidator(QtGui.QIntValidator())
+        # self.pdTChan.setValidator(QtGui.QIntValidator())
+        # self.binEntry.setValidator(QtGui.QIntValidator())
+        # self.freqEntry.setValidator(QtGui.QIntValidator())
+        # self.wSizeEntry.setValidator(QtGui.QIntValidator())
+        # self.savePerEntry.setValidator(QtGui.QIntValidator())
+        # self.avgDD.currentText()
+        # self.saveLocEntry.setValidator(QtGui.QIntValidator())
+
         # Parameters
-        sampleinterval=0.01
-        timewindow=10.
-        size=(600, 350)
+        sampleinterval=1/2000 # 0.01
+        timewindow=1.
         self.interval = int(sampleinterval * 1000)
         self.bufsize = int(timewindow / sampleinterval)
         self.x = np.linspace(-timewindow, 0.0, self.bufsize)
@@ -51,13 +73,28 @@ class MainWindow(uiclass, baseclass):
         self.b3 = collections.deque([0.0] * self.bufsize, self.bufsize)
 
         # Create signal
-        # self.request_graph_update = QtCore.pyqtSignal(np.ndarray)
-        # self.request_graph_update.connect(self.updatePlots, args)
         self.sig = SignalCommunicate()
         self.sig.request_graph_update.connect(self.updateGraphs)
 
-        self.iThread = InstrumentThread(func=self.updateData)
+        # Create thread
+        self.getSettings()
+        self.iThread = InstrumentThread(func=self.updateData, settings=self.settings1)
 
+        self.__formatPlots()
+
+        self.avgDD.addItem('True')
+        self.avgDD.addItem('False')
+
+        self.saving = False
+        self.monit = False
+        self.stime = time.time()
+
+        return
+
+    def __formatPlots(self):
+        """ Format plots for __init__() """
+        size=(600, 350)
+        
         # PyQtGraph photodiode plot
         self.photodiodeGraph.setTitle("Photodiode Data")
         self.photodiodeGraph.resize(*size)
@@ -82,43 +119,61 @@ class MainWindow(uiclass, baseclass):
         self.tempGraph.setLabel('bottom', 'Time', 's')
         self.tempCurvePd = self.tempGraph.plot(self.x, self.b1, pen=(255, 0, 0))
         self.tempCurveTp = self.tempGraph.plot(self.x, self.b3, pen=(255, 0, 0))
+        return None
 
     def startPlots(self):
-        self.iThread = InstrumentThread(func=self.updateData)
+        """ Start the measurement and live plotting """
+        self.getSettings()
+        self.iThread = InstrumentThread(func=self.updateData, settings = self.settings1)
         self.iThread.start()
-        # for plot in self.plots:
-        #     plot.start()
-        pass
+        return None
 
     def stopPlots(self):
+        """ Stop the measurement and live plotting """
         self.iThread.terminate()
-        # for plot in self.plots:
-        #     plot.stop()
         return None
 
     def startMonitor(self):
+        """ Start monitor button action """
         self.startMonitorButton.setDisabled(True)
         self.stopMonitorButton.setDisabled(False)
+        self.monit = True
         self.startPlots()
         return None
 
     def stopMonitor(self):
+        """ Stop monitor button action """
         self.startMonitorButton.setDisabled(False)
         self.stopMonitorButton.setDisabled(True)
+        if self.saving == True:
+            self.stopRecord()
+        self.monit = False
         self.stopPlots()
         return None
 
     def startRecord(self):
+        """ Start record button action """
         self.startRecordButton.setDisabled(True)
         self.stopRecordButton.setDisabled(False)
+        if self.monit == False:
+            self.startMonitor()
+
+        # Get settings
+        self.savePer = int(self.savePerEntry.text())
+        self.saveName = '/'.join([self.saveLocEntry.text(),'save_data.csv'])
+
+        self.saving = True
         return None
 
     def stopRecord(self):
+        """ Stop record button action """
         self.startRecordButton.setDisabled(False)
         self.stopRecordButton.setDisabled(True)
+        self.saving = False
         return None
 
     def updateGraphs(self):
+        """ Update the plots in the live gui """
         self.pdCurve.setData(self.x, self.b0)
         self.tpCurve.setData(self.x, self.b2)
         self.tempCurvePd.setData(self.x, self.b1)
@@ -126,83 +181,172 @@ class MainWindow(uiclass, baseclass):
         return None
 
     def updateData(self, data):
+        """ Update the data from the lastest measurement """
         self.b0.extend(data[0])
         self.b1.extend(data[1])
         self.b2.extend(data[2])
         self.b3.extend(data[3])
-        # print(ret[2])
-        # print('Callback')
-        # Update curve objects
-        # self.pdCurve.setData(self.x, self.b0)
-        # self.tpCurve.setData(self.x, self.b2)
-        self.sig.request_graph_update.emit()
         
+        if self.saving:
+            ctime = time.time()
+            if self.savePer < (ctime - self.stime):
+                self.stime = ctime
+                with open(self.saveName, 'a') as file1:
+                    linea = [str(ctime), str(self.b0[-1]), str(self.b1[-1]), str(self.b2[-1]), str(self.b3[-1])]
+                    line = ','.join(linea) + '\n'
+                    file1.write(line)
+                
+        # Emit a signal
+        self.sig.request_graph_update.emit()
+        return None
+    
+    def loadSettings(self):
+        """ Load settings from config file """
+        self.conf.loadCfg()
+
+        # Load address
+        index = self.devCombo.findText(self.conf.dAddr)
+        if index != -1:
+            self.devCombo.setCurrentIndex(index)
+
+        self.tpChan.clear()
+        self.tpChan.insert(self.conf.tpChan)
+
+        self.tpTChan.clear()
+        self.tpTChan.insert(self.conf.tpTChan)
+
+        self.pdChan.clear()
+        self.pdChan.insert(self.conf.pdChan)
+
+        self.pdTChan.clear()
+        self.pdTChan.insert(self.conf.pdTChan)
+
+        self.binEntry.clear()
+        self.binEntry.insert(self.conf.binSize)
+
+        self.freqEntry.clear()
+        self.freqEntry.insert(self.conf.sampFreq)
+
+        self.wSizeEntry.clear()
+        self.wSizeEntry.insert(self.conf.pWindow)
+
+        self.savePerEntry.clear()
+        self.savePerEntry.insert(self.conf.sPeroid)
+        
+        # w2 = self.avgDD.currentText()
+        index = self.avgDD.findText(self.conf.sAvg)
+        if index != -1:
+            self.avgDD.setCurrentIndex(index)
+        
+        self.saveLocEntry.clear()
+        self.saveLocEntry.insert(self.conf.sLoc)
+
         pass
+
+    def saveSettings(self):
+        """ Save settings in text boxes """
+        self.conf.saveCfg()
+        pass
+
+    def getSettings(self):
+        """ Get settings from text boxes """
+        s1 = self.devCombo.currentText()
+        s2 = self.tpChan.text()
+        s3 = self.tpTChan.text()
+        s4 = self.pdChan.text()
+        s5 = self.pdTChan.text()
+
+        v1 = int(self.binEntry.text())
+        v2 = int(self.freqEntry.text())
+        v3 = self.wSizeEntry.text()
+
+        w1 = self.savePerEntry.text()
+        w2 = self.avgDD.currentText()
+        w3 = self.saveLocEntry.text()
+
+        self.settings = [s1, s2, s3, s4, s5, v1, v2, v3, w1, w2, w3]
+        self.settings1 = [s1, s2, s3, s4, s5, v1, v2]
+
+        # print(settings)
+
+        return None
 
 
 class InstrumentThread(threading.Thread):
-# class InstrumentThread(QtCore.QThread):
-    def __init__(self, func):
-        
+    def __init__(self, func, settings):
+        # Function that needs to be called from the class above.
         self.func = func
 
         # Thread object
         self._running_flag = False
         self.stop = threading.Event()
+        
+        # Create a thread object
         super().__init__(target=self.record)
-
-        # super().__init__()
-        # self.started.connect(self.record)
-
         self.daemon = True
 
-        # Set channels
-        self.setChans()
+        self.chan = settings[0]
+        self.pdChan = settings[3]
+        self.thermoChan = settings[1]
+        self.pdTempChan = settings[4]
+        self.thermoTempChan = settings[2]
+        self.sper = settings[5]
+        self.sampleFreq = settings[6]
 
-        # QTimer
-        self.timer = QtCore.QTimer()
+        # Set channels
+        # self.setChans()
 
         return
 
     def terminate(self):
+        ''' Set stop event '''
         self.stop.set()
+        return None
 
-    def setChans(self, chan='Dev4', pdChan='ai1', thermoChan='ai3', pdTempChan='ai0', thermoTempChan='ai4'):
+    def setChans(self, chan='Dev4', pdChan='ai1', thermoChan='ai3', pdTempChan='ai0', thermoTempChan='ai4', sper=10, sampleFreq=2000):
+        '''
+        Inputs
+            - chan
+            - pdChan
+            - thermoChan
+            - pdTempChan
+            - thermoTempChan
+            - sper
+            - sampleFreq
+        '''
         # Save daq channel settings
         self.chan = chan
         self.pdChan = pdChan
         self.thermoChan = thermoChan
         self.pdTempChan = pdTempChan
         self.thermoTempChan = thermoTempChan
+        self.sper = sper
+        self.sampleFreq = sampleFreq
         return None
 
     def record(self):
         """ Method to start data collection """
         self.stop.clear()
-
         self.sper = 10 # sample period
         self.sampleFreq = 2000
 
         # Collect data
         with nidaqmx.Task() as task:
-
+            # Create tasks
             task.ai_channels.add_ai_voltage_chan('/'.join([self.chan, self.pdChan]), terminal_config=TerminalConfiguration.DIFF)
             # task.ai_channels.add_ai_voltage_chan('/'.join([self.chan, self.pdTempChan]), terminal_config=TerminalConfiguration.DIFF)
             task.ai_channels.add_ai_thrmcpl_chan('/'.join([self.chan, self.pdTempChan]), units=TemperatureUnits.DEG_C, thermocouple_type=ThermocoupleType.J)
-
             task.ai_channels.add_ai_voltage_chan('/'.join([self.chan, self.thermoChan]), terminal_config=TerminalConfiguration.DIFF)
             # task.ai_channels.add_ai_voltage_chan('/'.join([self.chan, self.thermoTempChan]), terminal_config=TerminalConfiguration.DIFF)
             task.ai_channels.add_ai_thrmcpl_chan('/'.join([self.chan, self.thermoTempChan]), units=TemperatureUnits.DEG_C, thermocouple_type=ThermocoupleType.J)
 
+            # Set task timing
             task.timing.cfg_samp_clk_timing(self.sampleFreq, sample_mode=AcquisitionType.CONTINUOUS)
 
             def callback(task_handle, every_n_samples_event_type, number_of_samples, callback_data):
                 """Callback function for reading singals."""
-                ret = task.read(number_of_samples_per_channel=self.sper)
-                
+                ret = task.read(number_of_samples_per_channel=self.sper)    
                 self.func(ret)
-                # Do the thing
-
                 return 0
 
             task.register_every_n_samples_acquired_into_buffer_event(self.sper, callback)
@@ -210,7 +354,6 @@ class InstrumentThread(threading.Thread):
             
             while self.stop.is_set() == False:
                 self.stop.wait(100)
-            # print('Stopped')
 
         return None
 
---
+++ yamlParser.py
@@ -0,0 +1,99 @@
+import yaml
+import os
+
+class Consts:
+    # Channels
+    dAddr = 'Dev4'
+    pdChan = 'ai2'
+    pdTChan = 'ai4'
+    tpChan = 'ai1'
+    tpTChan = 'ai3'
+
+    # Plot Settings
+    binSize = '1'
+    sampFreq = '2000'
+    pWindow = '1'
+
+    # Save Settings
+    sPeriod = '10'
+    sAvg = 'True'
+    sLoc = '.'
+
+    def get(self):
+        r = [self.dAddr, self.pdChan, self.pdTChan, self.tpChan, self.tpTChan, self.binSize, self.sampFreq, self.pWindow, self.sPeriod, self.sAvg, self.sLoc]
+        return r
+    
+class Cfg(Consts):
+    filename = 'settings.yml'
+
+    def createDefault(self):
+        """ Create default config file """
+        self.set(['Dev4','ai1','ai3','ai0','ai4','10','2000','1','10','False','.'])
+        self.settings = yaml.safe_load(inp)
+        with open(self.filename, 'w') as file:
+            yaml.dump(self.settings, file)
+        return None
+    
+    def exists(self):
+        """ Check if config file exists """
+        if os.path.isfile(self.filename):
+            return True
+        else:
+            return False
+
+    def get2(self):
+        self.dAddr = self.settings['daq']['chan']
+        self.pdChan = self.settings['daq']['pdChan']
+        self.pdTChan = self.settings['daq']['pdTempChan']
+        self.tpChan = self.settings['daq']['thermoChan']
+        self.tpTChan = self.settings['daq']['thermoTempChan']
+
+        self.binSize = self.settings['aqc']['bin']
+        self.sampFreq = self.settings['aqc']['freq']
+        self.pWindow = self.settings['aqc']['window']
+
+        self.sPeriod = self.settings['save']['per']
+        self.sAvg = self.settings['save']['avg']
+        self.sLoc = self.settings['save']['loc']
+        return None
+
+    def loadCfg(self):
+        """ Load config file """
+        if(self.exists()):
+            with open(self.filename, 'r') as file:
+                self.settings = yaml.safe_load(file)
+        else:
+            self.createDefault()
+        self.get2()
+        return None
+
+    def set(self, s):
+        """ Create config file object """
+        inp = """\
+        daq:
+            chan: {}
+            pdChan: {}
+            thermoChan: {}
+            pdTempChan: {}
+            thermoTempChan: {}
+        aqc:
+            bin: {}
+            freq: {}
+            window: {}
+        save:
+            per: {}
+            avg: {}
+            loc: {}
+        """.format(s[0], s[1], s[2], s[3], s[4], s[5], s[6], s[7], s[8], s[9], s[10])
+        self.settings = yaml.safe_load(inp)
+        return None
+
+    def saveCfg(self):
+        """ Save config file to file """
+        with open(self.filename, 'w') as file:
+            yaml.dump(self.settings, file)
+        return None
+
+    def print(self):
+        print(self.settings)
+        return None
